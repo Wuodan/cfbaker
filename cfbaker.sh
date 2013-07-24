4a826166c8df78a67aef080decc71f060220e9de
Stefan Kuhn <wuodan0@gmail.com>
2013-07-24 14:11:53 +0200

    Update cfbaker.sh

--- cfbaker.sh
+++ cfbaker.sh
@@ -2,7 +2,7 @@
 #
 # copy manually edited config files
 #
-# Copyright (c) 2010 Stefan Kuhn <stefan.kuhn@hispeed.ch>
+# Copyright (c) 2010 Stefan Kuhn
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
